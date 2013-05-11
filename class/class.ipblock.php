242d12f45d1b57f958d8859490d25362311c54fc
IPblock <loveman24@gmail.com>
2013-05-12 01:39:54 +0600

    Second Version

--- class/class.ipblock.php
+++ class/class.ipblock.php
@@ -5,12 +5,6 @@ class Ipblock{
     }
     
     public function add($ip,$sid){
-        $query_test = "SELECT * FROM iptable WHERE sid='$sid'";
-        $result_test = mysql_query($query_test);
-        if(mysql_num_rows($result_test)>0){
-            exit;
-        }
-        else {
             $query_test2 = "SELECT * FROM iptable WHERE ip='$ip'";
             $result_test2 = mysql_query($query_test2);
             if(mysql_num_rows($result_test2)<2){
@@ -21,7 +15,5 @@ class Ipblock{
                 header("Location: http://www.google.com");
             }
         }
-    }
-    
 }
 ?>
